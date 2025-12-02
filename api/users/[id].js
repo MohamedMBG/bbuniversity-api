36b28c127894064cbdc0c157fbbcbdd6e6ca84ae
MohamedMBG <mohamedbaghdad.me@gmail.com>
2025-12-02 03:26:18 +0100

    fixing

--- api/users/[id].js
+++ api/users/[id].js
@@ -1,5 +1,6 @@
 // api/users/[id].js
 const getDB = require('../../_db');
+const { ObjectId } = require('mongodb');
 
 module.exports = async (req, res) => {
   const { id } = req.query;
@@ -13,15 +14,30 @@ module.exports = async (req, res) => {
     return res.end();
   }
 
-  // ---------- GET /api/users/:id ----------
+  // ---------- GET USER ----------
   if (req.method === 'GET') {
     try {
       const db = await getDB();
-      const user = await db.collection('users').findOne({ _id: id }); // ✅ id = uid
+
+      // on construit un OR sur _id, uid, email
+      const orClauses = [
+        { _id: id },
+        { uid: id },
+        { email: id }
+      ];
+
+      // si jamais tu as des users avec ObjectId(_id)
+      if (/^[0-9a-fA-F]{24}$/.test(id)) {
+        orClauses.push({ _id: new ObjectId(id) });
+      }
+
+      const user = await db.collection('users').findOne({
+        $or: orClauses
+      });
 
       if (!user) {
         res.statusCode = 404;
-        return res.end(JSON.stringify({ message: 'User not found' }));
+        return res.end(JSON.stringify({ message: 'User not found', id }));
       }
 
       res.setHeader('Content-Type', 'application/json');
@@ -34,7 +50,7 @@ module.exports = async (req, res) => {
     }
   }
 
-  // ---------- PUT /api/users/:id ----------
+  // ---------- UPDATE USER ----------
   if (req.method === 'PUT') {
     try {
       const db = await getDB();
@@ -43,11 +59,19 @@ module.exports = async (req, res) => {
       req.on('data', chunk => { body += chunk.toString(); });
       req.on('end', async () => {
         const updates = JSON.parse(body || '{}');
+        delete updates._id; // on ne change jamais l'_id
 
-        delete updates._id; // on ne laisse pas changer l'id
+        const orClauses = [
+          { _id: id },
+          { uid: id },
+          { email: id }
+        ];
+        if (/^[0-9a-fA-F]{24}$/.test(id)) {
+          orClauses.push({ _id: new ObjectId(id) });
+        }
 
         const result = await db.collection('users').updateOne(
-          { _id: id },
+          { $or: orClauses },
           { $set: updates }
         );
 
@@ -63,8 +87,9 @@ module.exports = async (req, res) => {
       res.statusCode = 500;
       return res.end(JSON.stringify({ message: 'Server error', error: err.message }));
     }
-  } else {
-    res.statusCode = 405;
-    return res.end('Method Not Allowed');
+    return;
   }
+
+  res.statusCode = 405;
+  return res.end('Method Not Allowed');
 };
